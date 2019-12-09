70e0f925ef8f6a93c35f8500e5574a74b890beda
Bhavya <bhavyashah9612@gmail.com>
2019-12-09 14:22:55 -0500

    logout done

--- app/src/main/java/com/example/grocerymark/Home_Activity.java
+++ app/src/main/java/com/example/grocerymark/Home_Activity.java
@@ -6,13 +6,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Home_Activity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView freshco, nofrill, loblaws, walma;
+    Button logOut;
     TextView gotomap;
 
     @Override
@@ -27,34 +31,50 @@ public class Home_Activity extends AppCompatActivity implements View.OnClickList
         loblaws = findViewById(R.id.loblaw);
         walma = findViewById(R.id.walmart);
         gotomap = findViewById(R.id.search_loc);
+        logOut = findViewById(R.id.logout);
+
+        freshco.setOnClickListener(this);
+        nofrill.setOnClickListener(this);
+        loblaws.setOnClickListener(this);
+        walma.setOnClickListener(this);
+        logOut.setOnClickListener(this);
 
     }
+
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.freshco:
-                Toast.makeText(this, "You Clicked Freshco", Toast.LENGTH_SHORT).show();
-                Intent i1 = new Intent(getApplicationContext(), Category_Activity.class);
+                Toast.makeText(getApplicationContext(), "You Clicked Freshco", Toast.LENGTH_SHORT).show();
+                Intent i1 = new Intent(this, Category_Activity.class);
                 i1.putExtra("Click","Freshco");
                 startActivity(i1);
                 break;
             case R.id.nofrill:
-                Toast.makeText(this, "You Clicked Nofrill", Toast.LENGTH_SHORT).show();
-                Intent i2 =new Intent(getApplicationContext(), Category_Activity.class);
+                Toast.makeText(getApplicationContext(), "You Clicked Nofrill", Toast.LENGTH_SHORT).show();
+                Intent i2 =new Intent(this, Category_Activity.class);
                 i2.putExtra("Click","Nofrill");
                 startActivity(i2);
                 break;
             case R.id.loblaw:
-                Toast.makeText(this, "You Clicked Loblaw", Toast.LENGTH_SHORT).show();
-                Intent i3 =new Intent(getApplicationContext(), Category_Activity.class);
+                Toast.makeText(getApplicationContext(), "You Clicked Loblaw", Toast.LENGTH_SHORT).show();
+                Intent i3 =new Intent(this, Category_Activity.class);
                 i3.putExtra("Click","Loblaw");
                 startActivity(i3);
                 break;
             case R.id.walmart:
-                Toast.makeText(this, "You Clicked Walmart", Toast.LENGTH_SHORT).show();
-                Intent i4 =new Intent(getApplicationContext(), Category_Activity.class);
+                Toast.makeText(getApplicationContext(), "You Clicked Walmart", Toast.LENGTH_SHORT).show();
+                Intent i4 =new Intent(this, Category_Activity.class);
                 i4.putExtra("Click","Walmart");
                 startActivity(i4);
+                break;
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getApplicationContext(), "Successfully LogOut", Toast.LENGTH_SHORT).show();
+                Intent i5 =new Intent(this, SignOn_activity.class);
+                //i5.putExtra("Click","Walmart");
+                startActivity(i5);
+                finish();
                 break;
         }
     }
